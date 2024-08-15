c9373e21fcf19ec5e6c87d5524ae4c03d564ef97
VSat08 <satyam2001verma@gmail.com>
2024-08-15 18:02:31 +0530

    Enbled hot reloading

--- react-docker/vite.config.ts
+++ react-docker/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    hmr: true, // This enables HMR
+    watch: {
+      usePolling: true, // This helps with some Docker setups
+    },
+  },
+});
